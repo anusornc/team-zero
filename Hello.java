f7227c51e83f218d4f4097cf60608befe20195e3
nanabyp <juvery.byp2536@gmail.com>
2016-03-30 15:25:46 +0700

    Update Hello.java
    ทดลองสร้างไฟล์

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String args[]) {
-    String myname = "sek ka tik";
+    String myname = "benyapa chutima";
     System.out.println("My name is" + myname);
   }
 }
